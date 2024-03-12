8fdb8d6211625172d3a8efbb4a05c960c398ae3f
Kirill <zhukov.kg@phystech.edu>
2024-03-12 14:27:36 +0300

    Add saving theme state with shared_preferences

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,15 @@ import 'package:provider/provider.dart';
 
 import 'package:news/router/router.dart';
 import 'package:news/theme/theme_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = await SharedPreferences.getInstance();
+
   runApp(ChangeNotifierProvider(
-    create: (context) => ThemeProvider(),
+    create: (context) => ThemeProvider(prefs),
     child: const MyApp(),
   ));
 }
--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -1,19 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:news/theme/theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeDataPreferences {
+  static const String key = "themeData";
+  static const String lightMode = "lightMode";
+  static const String darkMode = "darkMode";
+}
 
 class ThemeProvider with ChangeNotifier {
-  ThemeData _themeData =
-      SchedulerBinding.instance.platformDispatcher.platformBrightness ==
-              Brightness.light
-          ? lightMode
-          : darkMode;
+  final SharedPreferences _prefs;
+  late ThemeData _themeData;
+
+  void _setThemeDataPreferences() {
+    String value;
+    if (_themeData == lightMode) {
+      value = ThemeDataPreferences.lightMode;
+    } else {
+      value = ThemeDataPreferences.darkMode;
+    }
+
+    _prefs.setString(ThemeDataPreferences.key, value);
+  }
+
+  ThemeData _getInitializeThemeData() {
+    String? value = _prefs.getString(ThemeDataPreferences.key);
+    if (value == null) {
+      final platformBrightness = SchedulerBinding.instance.platformDispatcher.platformBrightness;
+      bool isDarkMode = platformBrightness == Brightness.dark;
+      if (isDarkMode) {
+        return darkMode;
+      }
+      return lightMode;
+    }
+    if (value == ThemeDataPreferences.lightMode) {
+      return lightMode;
+    }
+    return darkMode;
+  }
+
+  ThemeProvider(this._prefs) {
+    _themeData = _getInitializeThemeData();
+    notifyListeners();
+    _setThemeDataPreferences();
+  }
 
   ThemeData get themeData => _themeData;
 
   set themeData(ThemeData themeData) {
     _themeData = themeData;
     notifyListeners();
+    _setThemeDataPreferences();
   }
 
   void toggleTheme() {
@@ -22,6 +61,8 @@ class ThemeProvider with ChangeNotifier {
     } else {
       themeData = lightMode;
     }
+    notifyListeners();
+    _setThemeDataPreferences();
   }
 
   bool isDarkMode() {
